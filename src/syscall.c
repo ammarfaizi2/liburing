0d134d616448e1603fe4cbfd03bd9a0128ab9cad
Ammar Faizi <ammar.faizi@students.amikom.ac.id>
2021-10-01 21:17:06 +0700

    src/{queue,register,setup}: Don't use `__sys_io_uring*`
    Don't use `__sys_io_uring*` for liburing internal. These functions
    are now supposed for user backward compatibility. Instead, we use
    `____sys_io_uring*` (4 underscores). These are `static inline`
    functions that wrap the `errno` variable with the kernel style return
    value (directly returns negative error code when errors).
    
    The main purpose of this change is to make it possible to remove the
    `errno` variable dependency from liburing C sources, so that later
    we will be able to implement no libc environment which doesn't use
    `errno` variable at all.
    
    Cc: Bedirhan KURT <windowz414@gnuweeb.org>
    Cc: Louvian Lyndal <louvianlyndal@gmail.com>
    Signed-off-by: Ammar Faizi <ammar.faizi@students.amikom.ac.id>

--- src/syscall.c
+++ src/syscall.c
@@ -25,13 +25,13 @@ int __sys_io_uring_setup(unsigned entries, struct io_uring_params *p)
 int __sys_io_uring_enter2(int fd, unsigned to_submit, unsigned min_complete,
 			 unsigned flags, sigset_t *sig, int sz)
 {
-	return syscall(__NR_io_uring_enter, fd, to_submit, min_complete,
-			flags, sig, sz);
+	return syscall(__NR_io_uring_enter, fd, to_submit, min_complete, flags,
+		       sig, sz);
 }
 
 int __sys_io_uring_enter(int fd, unsigned to_submit, unsigned min_complete,
 			 unsigned flags, sigset_t *sig)
 {
 	return __sys_io_uring_enter2(fd, to_submit, min_complete, flags, sig,
-					_NSIG / 8);
+				     _NSIG / 8);
 }
