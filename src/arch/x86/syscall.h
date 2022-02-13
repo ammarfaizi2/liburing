cbf8b2ef47698e26a5e9cbace2d8b8f550b314e1
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-02-13 22:10:17 +0700

    arch/x86: Add x86 32-bit native syscall support
    This adds x86 32-bit native syscall support.
    
    Note for __do_syscall6() macro
    -------------------------------
    __do_syscall6() on x86 32-bit is a mess. Both Clang and GCC
    cannot use %ebp in the clobber list and "r" constraint without
    `-fomit-frame-pointer`
    
    Current implementation:
    
    For clang (the Assembly statement can't clobber %ebp):
      1) Push %ebp to preserve its value on the stack.
      2) Move the 6th argument of syscall from memory to %ebp.
      3) Do the syscall (int $0x80).
      4) Pop %ebp to restore the %ebp value.
    
    For GCC, fortunately it has a #pragma that can force a specific
    function to be compiled with -fomit-frame-pointer, so it can use
    "r"(var) where var is a variable bound to %ebp.
    
    If you have a better approach for this, please mail me.
    
       To: io-uring Mailing List <io-uring@vger.kernel.org>
       Cc: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/arch/x86/syscall.h
+++ src/arch/x86/syscall.h
@@ -142,16 +142,181 @@
 
 #else /* #if defined(__x86_64__) */
 
-/*
- * For x86 (32-bit), fallback to libc wrapper.
- * We can't use CONFIG_NOLIBC for x86 (32-bit) at the moment.
- *
- * TODO: Add x86 (32-bit) nolibc support.
+/**
+ * Note for syscall registers usage (x86, 32-bit):
+ *   - %eax is the syscall number.
+ *   - %eax is also the return value.
+ *   - %ebx is the 1st argument.
+ *   - %ecx is the 2nd argument.
+ *   - %edx is the 3rd argument.
+ *   - %esi is the 4th argument.
+ *   - %edi is the 5th argument.
+ *   - %ebp is the 6th argument.
  */
-#ifdef CONFIG_NOLIBC
-	#error "x86 (32-bit) is currently not supported for nolibc builds"
-#endif
-#include "../generic/syscall.h"
+
+#define __do_syscall0(NUM) ({			\
+	intptr_t eax;				\
+						\
+	__asm__ volatile(			\
+		"int	$0x80"			\
+		: "=a"(eax)	/* %eax */	\
+		: "a"(NUM)	/* %eax */	\
+		: "memory"			\
+	);					\
+	eax;					\
+})
+
+#define __do_syscall1(NUM, ARG1) ({		\
+	intptr_t eax;				\
+						\
+	__asm__ volatile(			\
+		"int	$0x80"			\
+		: "=a"(eax)	/* %eax */	\
+		: "a"(NUM),	/* %eax */	\
+		  "b"((ARG1))	/* %ebx */	\
+		: "memory"			\
+	);					\
+	eax;					\
+})
+
+#define __do_syscall2(NUM, ARG1, ARG2) ({	\
+	intptr_t eax;				\
+						\
+	__asm__ volatile(			\
+		"int	$0x80"			\
+		: "=a" (eax)	/* %eax */	\
+		: "a"(NUM),	/* %eax */	\
+		  "b"((ARG1)),	/* %ebx */	\
+		  "c"((ARG2))	/* %ecx */	\
+		: "memory"			\
+	);					\
+	eax;					\
+})
+
+#define __do_syscall3(NUM, ARG1, ARG2, ARG3) ({	\
+	intptr_t eax;				\
+						\
+	__asm__ volatile(			\
+		"int	$0x80"			\
+		: "=a" (eax)	/* %eax */	\
+		: "a"(NUM),	/* %eax */	\
+		  "b"((ARG1)),	/* %ebx */	\
+		  "c"((ARG2)),	/* %ecx */	\
+		  "d"((ARG3))	/* %edx */	\
+		: "memory"			\
+	);					\
+	eax;					\
+})
+
+#define __do_syscall4(NUM, ARG1, ARG2, ARG3, ARG4) ({	\
+	intptr_t eax;					\
+							\
+	__asm__ volatile(				\
+		"int	$0x80"				\
+		: "=a" (eax)	/* %eax */		\
+		: "a"(NUM),	/* %eax */		\
+		  "b"((ARG1)),	/* %ebx */		\
+		  "c"((ARG2)),	/* %ecx */		\
+		  "d"((ARG3)),	/* %edx */		\
+		  "S"((ARG4))	/* %esi */		\
+		: "memory"				\
+	);						\
+	eax;						\
+})
+
+#define __do_syscall5(NUM, ARG1, ARG2, ARG3, ARG4, ARG5) ({	\
+	intptr_t eax;						\
+								\
+	__asm__ volatile(					\
+		"int	$0x80"					\
+		: "=a" (eax)	/* %eax */			\
+		: "a"(NUM),	/* %eax */			\
+		  "b"((ARG1)),	/* %ebx */			\
+		  "c"((ARG2)),	/* %ecx */			\
+		  "d"((ARG3)),	/* %edx */			\
+		  "S"((ARG4)),	/* %esi */			\
+		  "D"((ARG5))	/* %edi */			\
+		: "memory"					\
+	);							\
+	eax;							\
+})
+
+/*
+ * __do_syscall6() on x86 32-bit is a mess.
+ *
+ * Both Clang and GCC cannot use %ebp in the clobber list and "r" constraint
+ * without -fomit-frame-pointer.
+ *
+ * Current implementation:
+ *
+ * For clang (the Assembly statement can't clobber %ebp):
+ *   1) Push %ebp to preserve its value on the stack.
+ *   2) Move the 6th argument of syscall from memory to %ebp.
+ *   3) Do the syscall (int $0x80).
+ *   4) Pop %ebp to restore the %ebp value.
+ *
+ * For GCC, fortunately it has #pragma that can force a specific function
+ * to be compiled with -fomit-frame-pointer, so it can use "r"(var) where
+ * var is a variable bound to %ebp.
+ *
+ * If you have a better approach for this, please mail me.
+ *
+ *   To: io-uring Mailing List <io-uring@vger.kernel.org>
+ *   Cc: Ammar Faizi <ammarfaizi2@gnuweeb.org>
+ *
+ */
+#if defined(__clang__)
+static inline intptr_t ____do_syscall6(intptr_t eax, intptr_t ebx, intptr_t ecx,
+				       intptr_t edx, intptr_t esi, intptr_t edi,
+				       intptr_t ebp)
+{
+	__asm__ volatile(
+		"pushl	%%ebp\n\t"
+		"movl	%[arg6], %%ebp\n\t"
+		"int	$0x80\n\t"
+		"popl	%%ebp\n\t"
+		: "=a" (eax)		/* %eax */
+		: "a"(eax),		/* %eax */
+		  "b"(ebx),		/* %ebx */
+		  "c"(ecx),		/* %ecx */
+		  "d"(edx),		/* %edx */
+		  "S"(esi),		/* %esi */
+		  "D"(edi),		/* %edi */
+		  [arg6]"m"(ebp)	/* %ebp */
+		: "memory"
+	);
+	return eax;
+}
+#else /* #if defined(__clang__) */
+#pragma GCC push_options
+#pragma GCC optimize "-fomit-frame-pointer"
+static inline intptr_t ____do_syscall6(intptr_t eax, intptr_t ebx, intptr_t ecx,
+				       intptr_t edx, intptr_t esi, intptr_t edi,
+				       intptr_t ebp)
+{
+	register intptr_t __ebp __asm__("ebp") = ebp;
+	__asm__ volatile(
+		"int	$0x80"
+		: "=a" (eax)	/* %eax */
+		: "a"(eax),	/* %eax */
+		  "b"(ebx),	/* %ebx */
+		  "c"(ecx),	/* %ecx */
+		  "d"(edx),	/* %edx */
+		  "S"(esi),	/* %esi */
+		  "D"(edi),	/* %edi */
+		  "r"(__ebp)	/* %ebp */
+		: "memory"
+	);
+	return eax;
+}
+#pragma GCC pop_options
+#endif /* #if defined(__clang__) */
+
+#define __do_syscall6(NUM, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6) (		\
+    ____do_syscall6((intptr_t) (NUM), (intptr_t) (ARG1), (intptr_t) (ARG2),	\
+                    (intptr_t) (ARG3), (intptr_t) (ARG4),			\
+                    (intptr_t) (ARG5), (intptr_t) (ARG6))			\
+)
 
 #endif /* #if defined(__x86_64__) */
 
